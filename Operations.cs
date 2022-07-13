9a079b9a1a660b0e3c18f41b4c136c989fe3e0b1
hagit worke <hagitw97@gmail.com>
2022-07-14 00:31:47 +0300

    Add project files.

---
+++ Operations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clal_HW
+{
+    class Operations
+    {
+        //Operation 1
+        public int PlusTHree(int input) 
+        {
+            return input + 3;
+        }
+
+        //Operation 2
+        public int MultiplyByFive(int input)
+        {
+            return input *5;
+        }
+
+        //Operation 3
+        public int DivideByTwo(int input)
+        {
+            return input/2;
+        }
+        //Operation 4
+        public int Operation4(int input)
+        {
+            try
+            {
+                string strInput = input.ToString();
+                if (strInput.Contains('7') && input % 7 == 0)
+                {
+                    throw new Exception();
+                }
+                else {
+                   return input + 7;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+    }
+}
---
+++ Step.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clal_HW
+{
+    class Step 
+    {
+        public int  Id { get; set; }
+        public string OperationName { get; set; }
+
+        public int NextIdIfOutputIsLessThan { get; set; }
+
+        public int NextIdIfOutputIsGreaterThan { get; set; }
+
+    }
+
+}
+
